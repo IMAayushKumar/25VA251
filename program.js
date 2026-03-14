a4880bfd22a35a622ff3972544545bc758c041a6
Aayush <ikps562@gmail.com>
2026-03-14 11:24:17 +0530

    A Basic program of sum and average

---
+++ program.js
@@ -0,0 +1,9 @@
+let marks=[23,34,45,56,67];
+
+for (let i = 0; i < marks.length; i++) {
+    const element = marks[i];
+     let sum=0;
+    sum=sum+element;   
+}
+ let avg=sum/5;
+    console.log(avg);
